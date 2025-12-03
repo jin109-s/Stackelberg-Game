6dff850b63cea6bf19f287b67aa1d2a49b79ccbd
jin109-s <zhiheng16@qq.com>
2025-12-03 16:19:56 +0800

    Add files via upload

---
+++ network_params.py
@@ -0,0 +1,16 @@
+# utils/network_params.py
+
+"""
+网络与能耗参数
+统一管理，方便调用
+"""
+
+# 上下行带宽 (KB/s)
+B_UP_EDGE = 5000.0
+B_DOWN_EDGE = 5000.0
+B_UP_CLOUD = 1000.0
+B_DOWN_CLOUD = 1000.0
+
+# 发射功率 / 接收功率
+P_TX = 0.1
+P_RX = 0.05
