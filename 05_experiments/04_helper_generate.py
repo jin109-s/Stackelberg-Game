21617697cf34385c945fee7515f1c517f0eb5add
jin109-s <zhiheng16@qq.com>
2025-12-03 20:35:35 +0800

    Update 04_helper_generate.py

--- 05_experiments/04_helper_generate.py
+++ 05_experiments/04_helper_generate.py
@@ -5,10 +5,10 @@
 """
 
 import random
-from models.user_device import UserDevice
-from models.task import Task
-from models.edge_server import EdgeServer
-from models.cloud_server import CloudServer
+from 03_models.01_user_device import UserDevice
+from 03_models.00_task import Task
+from 03_models.02_edge_server import EdgeServer
+from 03_models.03_cloud_server import CloudServer
 
 
 def generate_user(user_id, num_tasks, local_factor=1.0, input_scale=1.0):
@@ -43,4 +43,5 @@ def build_edge_cloud(edge_factor=1.0):
 
     cloud = CloudServer(2.4e9, 6e-12)
     return edge, cloud
+
 
